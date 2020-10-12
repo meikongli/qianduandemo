096d151f6c7da73cf4b1100d64009e7a6c877130
meikongli <1535798792@qq.com>
2020-10-12 18:35:12 +0800

    提交

--- src/views/no3/no3.vue
+++ src/views/no3/no3.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>第三页面</h1>
+    <h1>第三页面   初始化调用 axios 获取后台数据 进行渲染  点击button按钮进行调用后台数据进行渲染 </h1>
+    <el-button  @click.native="getSelect">调用后端</el-button>
   </div>
 </template>
 
@@ -10,6 +11,41 @@ export default {
   data () {
     return {
     }
+  },
+  //方法
+  methods: {
+    getSelect(){
+      this.axios.post('/user', {
+          name: 'name',
+          pwd: 'pwd'
+        })
+        .then(function (response) {
+        //成功方法
+          console.log(response);
+        })
+        .catch(function (error) {
+        //失败方法
+          console.log(error);
+        });
+
+    }
+
+  },
+  //初始化的时候调用 axios 
+  mounted(){
+    let  params={};
+    params.name1="1"
+    params.name2="2"
+    params.name3="3"
+    this.axios.post('/user', params) .then(function (response) {
+      //成功方法
+        console.log(response);
+      })
+      .catch(function (error) {
+        //失败方法
+        console.log(error);
+      });
+
   }
 }
 </script>
